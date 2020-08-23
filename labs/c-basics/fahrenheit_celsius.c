74979a9dc4df28bda9b6f65403a7b81324413584
Luis Palomino Trevilla <luispalominot3@gmail.com>
2020-08-23 01:09:16 -0500

    Completed fahrenheit to celsius program
    Signed-off-by: Luis Palomino Trevilla <luispalominot3@gmail.com>

--- labs/c-basics/fahrenheit_celsius.c
+++ labs/c-basics/fahrenheit_celsius.c
@@ -1,17 +1,38 @@
 #include <stdio.h>
-
-#define   LOWER  0       /* lower limit of table */
-#define   UPPER  300     /* upper limit */
-#define   STEP   20      /* step size */
+#include <stdlib.h>
 
 /* print Fahrenheit-Celsius table */
 
-int main()
+float toCelsius(int);
+void printConversion(int);
+
+int main(int argc, char **argv)
 {
-    int fahr;
+  if (argc == 2) {
+    printConversion(atoi(argv[1]));
+  } else if (argc == 4) {
+    int step = atoi(argv[3]);
+    if (step <= 0) {
+      printf("Step must be greater than 0\n");
+      exit(1);
+    }
 
-    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
-	printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+    for (int fahr = atoi(argv[1]); fahr <= atoi(argv[2]); fahr = fahr + step) {
+      printConversion(fahr);
+    }
+  } else {
+    printf("Expected parameter(s) for simple conversion or range conversion\n");
+    exit(1);
+  }
 
-    return 0;
+  return 0;
+}
+
+void printConversion(int fahr) {
+  printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, toCelsius(fahr));
+}
+
+float toCelsius(int fahr)
+{
+  return (5.0 / 9.0) * (fahr - 32);
 }
