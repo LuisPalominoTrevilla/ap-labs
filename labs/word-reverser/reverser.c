25862d045aeb94ea0527bca8ce8c71e6f979de46
Luis Palomino Trevilla <luispalominot3@gmail.com>
2020-08-27 12:45:39 -0500

    Finished word reverser program
    Signed-off-by: Luis Palomino Trevilla <luispalominot3@gmail.com>

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
 
-int main(){
-    // Place your magic here
-    return 0;
+#define MAX_ARRAY_LENGTH 100
+
+void reverse(int, char *);
+
+int main()
+{
+  int i = 0;
+  char c;
+  char line[MAX_ARRAY_LENGTH];
+
+  while ((c = getchar()) != EOF)
+  {
+    if (c == '\n')
+    {
+      line[i] = '\0';
+      reverse(i, line);
+      printf("%s\n", line);
+      i = 0;
+      continue;
+    }
+
+    line[i++] = c;
+  }
+
+  return 0;
+}
+
+void reverse(int n, char *arr)
+{
+  char tmp;
+
+  for (int i = 0; i < n / 2; i++)
+  {
+    tmp = arr[i];
+    arr[i] = arr[n - i - 1];
+    arr[n - i - 1] = tmp;
+  }
 }
