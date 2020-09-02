3c98722bc5b3fdb44406989abfd9bb91b206d06d
Luis Palomino Trevilla <luispalominot3@gmail.com>
2020-09-02 18:48:16 -0500

    Finished str lib lab
    Signed-off-by: Luis Palomino Trevilla <luispalominot3@gmail.com>

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -1,5 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+int mystrlen(char *str);
+char *mystradd(char *origin, char *addition);
+int mystrfind(char *origin, char *substr);
+int mystrcmp(char *str1, char *str2);
+
+int main(int argc, char **argv)
+{
+  char *mode, *p1, *p2;
+  if (argc != 4)
+  {
+    printf("Error: Wrong number of arguments received\n");
     return 0;
+  }
+
+  mode = argv[1];
+  p1 = argv[2];
+  p2 = argv[3];
+  if (mystrcmp(mode, "-add"))
+  {
+    char *newStr = mystradd(p1, p2);
+    printf("Initial Length \t: %d\n", mystrlen(p1));
+    printf("New String \t: %s\n", newStr);
+    printf("New Length \t: %d\n", mystrlen(newStr));
+    free(newStr);
+  }
+  else if (mystrcmp(mode, "-find"))
+  {
+    int pos = mystrfind(p1, p2);
+    if (pos != -1)
+    {
+      printf("[%s] string was found at [%d] position\n", p2, pos);
+    }
+    else
+    {
+      printf("[%s] string was not found\n", p2);
+    }
+  }
+  else
+  {
+    printf("Option %s is not supported. Try -add or -find instead.\n", mode);
+  }
+
+  return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,67 @@
+#include <stdlib.h>
 
+int mystrlen(char *str)
+{
+  int len = 0;
+  while (*str != '\0')
+  {
+    len++;
+    str++;
+  }
 
-int mystrlen(char *str){
-    return 0;
+  return len;
 }
 
-char *mystradd(char *origin, char *addition){
-    return 0;
+char *mystradd(char *origin, char *addition)
+{
+  int origLen = mystrlen(origin);
+  int aditLen = mystrlen(addition);
+  int n = origLen + aditLen;
+
+  char *dest = (char *)malloc(n + 1);
+  for (int i = 0; i < n; i++)
+    dest[i] = i < origLen ? origin[i] : addition[i - origLen];
+
+  dest[n] = '\0';
+  return dest;
 }
 
-int mystrfind(char *origin, char *substr){
+int mystrfind(char *origin, char *substr)
+{
+  int i = 0, j = 0, s = 0;
+  int n = mystrlen(origin), slen = mystrlen(substr);
+  if (n < slen)
+    return -1;
+
+  while (j < n)
+  {
+    if (origin[j] != substr[s])
+    {
+      j = ++i;
+      s = 0;
+      continue;
+    }
+
+    j++;
+    s++;
+    if (s == slen)
+      return i;
+  }
+
+  return -1;
+}
+
+int mystrcmp(char *str1, char *str2)
+{
+  int n = mystrlen(str1);
+  if (n != mystrlen(str2))
     return 0;
+
+  for (int i = 0; i < n; i++)
+  {
+    if (str1[i] != str2[i])
+      return 0;
+  }
+
+  return 1;
 }
