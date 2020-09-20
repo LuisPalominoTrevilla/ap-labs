0efd83bfff654bbd2328a2cd894001fdb37921b3
Luis Palomino Trevilla <luispalominot3@gmail.com>
2020-09-20 15:52:28 -0500

    Finished slices and maps in go

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	count := make(map[string]int)
+	words := strings.Fields(s)
+	for _, word := range words {
+		count[word] += 1
+	}
+
+	return count
+}
+
 func main() {
+	wc.Test(WordCount)
 }
