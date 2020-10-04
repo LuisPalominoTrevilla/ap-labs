0271c67eb2e24bc65e59c5db6aa9817d6fcffeac
Luis Palomino Trevilla <luispalominot3@gmail.com>
2020-10-04 15:58:17 -0500

    Detec whether arg is valid directory

--- labs/file-dir-monitor/monitor.c
+++ labs/file-dir-monitor/monitor.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include "logger.h"
 
-int main(){
-    // Place your magic here
+int main(int argc, char **argv)
+{
+  char *directory;
+  initLogger("stdout");
+
+  if (argc < 2)
+  {
+    errorf("Usage: ./monitor.o [DIRNAME]\n");
     return 0;
+  }
+
+  directory = argv[1];
+  struct stat path_stat;
+  if (stat(directory, &path_stat) != 0 || !S_ISDIR(path_stat.st_mode))
+  {
+    errorf("%s is not a directory\n", directory);
+    return 0;
+  }
+
+  return 0;
 }
