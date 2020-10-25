d4f1416d0d2ace9baf71d62a2762efa6e7d2b63d
Luis Palomino Trevilla <luispalominot3@gmail.com>
2020-10-25 11:47:43 -0600

    Finished depth limiting web crawler
    Signed-off-by: Luis Palomino Trevilla <luispalominot3@gmail.com>

--- labs/web-crawler/web-crawler.go
+++ labs/web-crawler/web-crawler.go
@@ -13,6 +13,8 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -25,38 +27,73 @@ import (
 // enforce a limit of 20 concurrent requests.
 var tokens = make(chan struct{}, 20)
 
-func crawl(url string) []string {
-	fmt.Println(url)
+func crawl(link node) []node {
 	tokens <- struct{}{} // acquire a token
-	list, err := links.Extract(url)
+	list, err := links.Extract(link.url)
 	<-tokens // release the token
 
 	if err != nil {
 		log.Print(err)
 	}
-	return list
+	next := make([]node, 0, len(list))
+	for _, val := range list {
+		next = append(next, node{val, link.depth + 1})
+	}
+	return next
 }
 
 //!-sema
 
+type node struct {
+	url   string
+	depth int
+}
+
 //!+
 func main() {
-	worklist := make(chan []string)
+	if len(os.Args) < 4 {
+		log.Println("Usage: ./web-crawler -depth=<depth> -results=<file> <url>")
+		return
+	}
+
+	depth := flag.Int("depth", 0, "Web crawler depth")
+	results := flag.String("results", "results.txt", "The results output file")
+	flag.Parse()
+
+	fmt.Println(*depth, *results)
+
+	// Create output file and initialize buffered writer
+	f, err := os.Create(*results)
+	if err != nil {
+		panic(err)
+	}
+
+	w := bufio.NewWriter(f)
+	defer w.Flush()
+
+	worklist := make(chan []node)
 	var n int // number of pending sends to worklist
 
 	// Start with the command-line arguments.
 	n++
-	go func() { worklist <- os.Args[1:] }()
+	go func() {
+		worklist <- []node{node{os.Args[3], 0}}
+	}()
 
 	// Crawl the web concurrently.
 	seen := make(map[string]bool)
 	for ; n > 0; n-- {
 		list := <-worklist
 		for _, link := range list {
-			if !seen[link] {
-				seen[link] = true
+			if link.depth > *depth {
+				break
+			}
+
+			if !seen[link.url] {
+				seen[link.url] = true
 				n++
-				go func(link string) {
+				fmt.Fprintf(w, "%s\n", link.url)
+				go func(link node) {
 					worklist <- crawl(link)
 				}(link)
 			}
