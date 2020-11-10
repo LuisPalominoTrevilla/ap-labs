88a552fcfe5434bbf50b11b799c00d9e4f9c7280
Luis Palomino Trevilla <luispalominot3@gmail.com>
2020-11-10 05:31:41 +0000

    Completed pipeline exercise

--- labs/goroutines-vs-os-threads/pipeline.go
+++ labs/goroutines-vs-os-threads/pipeline.go
@@ -2,11 +2,28 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-func main() {
-	var maxStages, transitTime int
+// If program never consumes all memory, modify max number to test
+// a fixed amount of pipeline stages
+const maxAllowedStages = 10000000
 
-	fmt.Println("Maximum number of pipeline stages   : ", maxStages)
-	fmt.Println("Time to transit trough the pipeline : ", transitTime)
+func main() {
+	var maxStages int
+	ch := make(chan struct{})
+
+	defer func() {
+		start := time.Now()
+		for i := maxStages; i > 0; i-- {
+			ch <- struct{}{}
+		}
+		transitTime := time.Now().Sub(start).Seconds()
+		fmt.Println("Maximum number of pipeline stages   : ", maxStages)
+		fmt.Println("Time to transit trough the pipeline : ", transitTime, "seconds")
+	}()
+
+	for maxStages = 0; maxStages < maxAllowedStages; maxStages++ {
+		go func() { <-ch }()
+	}
 }
