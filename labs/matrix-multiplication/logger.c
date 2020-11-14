65c76436b94269ccb4bcef0ace3d771963b01c20
Luis Palomino Trevilla <luispalominot3@gmail.com>
2020-11-14 14:05:53 -0600

    Added function to read matrix from file
    Signed-off-by: Luis Palomino Trevilla <luispalominot3@gmail.com>

--- labs/matrix-multiplication/logger.c
+++ labs/matrix-multiplication/logger.c
@@ -1,23 +1,92 @@
 #include <stdio.h>
+#include <stdarg.h>
+#include <syslog.h>
+#include <string.h>
+#include <time.h>
 #include "logger.h"
 
-int initLogger(char *logType) {
-    printf("Initializing Logger on: %s\n", logType);
-    return 0;
+#define STDOUT 0
+#define SYSLOG 1
+
+#define BLACK 0
+#define RED 1
+#define YELLOW 3
+#define BLUE 4
+#define MAGENTA 5
+#define WHITE 7
+
+static int logOutput = STDOUT;
+
+int initLogger(char *logType)
+{
+  if (strcmp(logType, "stdout") == 0)
+  {
+    logOutput = STDOUT;
+  }
+  else if (strcmp(logType, "syslog") == 0)
+  {
+    openlog("Custom Logger -", LOG_CONS, LOG_USER);
+    logOutput = SYSLOG;
+  }
+  else
+  {
+    printf("Invalid log type %s. Defaulting to stdout\n", logType);
+  }
+
+  printf("Initializing Logger on: %s\n", logType);
+  return 0;
 }
 
-int infof(const char *format, ...) {
-    return 0;
+void __colorizeLog(int fg, int bg, int type, const char *title, const char *text, va_list args)
+{
+  if (logOutput == STDOUT)
+  {
+    time_t rawtime;
+    struct tm *timeinfo;
+
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+    printf("\x1b[%d;%dm [%d-%d-%dT%d:%d:%d] %s: \x1b[0m ", fg + 30, bg + 40, timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec, title);
+    vprintf(text, args);
+  }
+  else
+  {
+    vsyslog(type, text, args);
+  }
 }
 
-int warnf(const char *format, ...) {
-    return 0;
+int infof(const char *format, ...)
+{
+  va_list args;
+  va_start(args, format);
+  __colorizeLog(WHITE, BLUE, LOG_INFO, "INFO", format, args);
+  va_end(args);
+  return 0;
 }
 
-int errorf(const char *format, ...) {
-    return 0;
-}
-int panicf(const char *format, ...) {
-    return 0;
+int warnf(const char *format, ...)
+{
+  va_list args;
+  va_start(args, format);
+  __colorizeLog(BLACK, YELLOW, LOG_WARNING, "WARN", format, args);
+  va_end(args);
+  return 0;
 }
 
+int errorf(const char *format, ...)
+{
+  va_list args;
+  va_start(args, format);
+  __colorizeLog(WHITE, RED, LOG_ERR, "ERROR", format, args);
+  va_end(args);
+  return 0;
+}
+
+int panicf(const char *format, ...)
+{
+  va_list args;
+  va_start(args, format);
+  __colorizeLog(WHITE, MAGENTA, LOG_EMERG, "PANIC", format, args);
+  va_end(args);
+  return 0;
+}
--- labs/matrix-multiplication/multiplier.c
+++ labs/matrix-multiplication/multiplier.c
@@ -1,7 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "logger.h"
 
-int main(){
-    // Place your magic here
+#define ROWS 2000l
+#define COLS 2000l
+
+typedef long *mat;
+
+mat readMatrix(char *filename);
+
+int main(int argc, char **argv)
+{
+  initLogger("stdout");
+  mat matA = readMatrix("matA.dat");
+  if (matA == NULL)
+  {
+    errorf("Could not read matrix A file\n");
     return 0;
+  }
+
+  infof("Read and got %ld %ld\n", matA[0], matA[1]);
+
+  free(matA);
+  return 0;
+}
+
+mat readMatrix(char *filename)
+{
+  FILE *f = fopen(filename, "r");
+  if (f == NULL)
+  {
+    return NULL;
+  }
+
+  size_t n = ROWS * COLS;
+  mat matrix = (mat)malloc(sizeof(long) * n);
+  if (matrix == NULL)
+  {
+    panicf("Unable to allocate buffer\n");
+    exit(1);
+  }
+
+  char *buf[30];
+  size_t len = 30;
+  int i = 0;
+  while (getline(buf, &len, f) != -1 && i < n)
+  {
+    matrix[i++] = strtol(*buf, NULL, 10);
+  }
+
+  fclose(f);
+  return matrix;
 }
